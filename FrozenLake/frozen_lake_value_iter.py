97903da4de4c17400968022d8a891c17f269c619
zhubarb <berkan.sesen@gmail.com>
2021-04-03 19:23:24 +0100

    Frozen lake q value solution(s).

--- FrozenLake/frozen_lake_value_iter.py
+++ FrozenLake/frozen_lake_value_iter.py
@@ -7,38 +7,45 @@ import numpy as np
 
 
 def value_iteration(env, max_iterations=100000, lmbda=0.9):
-    stateValue = [0 for i in range(env.nS)]
+    stateValue = [0 for i in range(env.env.nS)]
     newStateValue = stateValue.copy()
-    for i in range(max_iterations):
-        for state in range(env.nS):
+    for iter in range(max_iterations):
+        for state in range(env.env.nS):
             action_values = []
-            for action in range(env.nA):
+            for action in range(env.env.nA):
                 state_value = 0
-                for i in range(len(env.P[state][action])):
-                    prob, next_state, reward, done = env.P[state][action][i]
+                for i in range(len(env.env.P[state][action])):
+                    prob, next_state, reward, done = env.env.P[state][action][i]
                     state_action_value = prob * (reward + lmbda * stateValue[next_state])
                     state_value += state_action_value
                 action_values.append(state_value)  # the value of each action
                 best_action = np.argmax(np.asarray(action_values))  # choose the action which gives the maximum value
                 newStateValue[state] = action_values[best_action]  # update the value of the state
-        if i > 1000:
+        if iter > 1000:
             if sum(stateValue) - sum(newStateValue) < 1e-04:  # if there is negligible difference break the loop
                 break
-                print(i)
+                print(iter)
         else:
             stateValue = newStateValue.copy()
     return stateValue
 
 
-def get_policy(env, stateValue, lmbda=0.9):
-    policy = [0 for i in range(env.nS)]
-    for state in range(env.nS):
+def get_policy(env, stateValue, gamma=0.9):
+    '''
+    Get optimal policy for s, based on the action that maximises long term reward (R+gamma*V(s'))
+    :param env: 
+    :param stateValue: 
+    :param gamma: 
+    :return: 
+    '''
+    policy = [0 for i in range(env.env.nS)]
+    for state in range(env.env.nS):
         action_values = []
-        for action in range(env.nA):
+        for action in range(env.env.nA):
             action_value = 0
-            for i in range(len(env.P[state][action])):
-                prob, next_state, r, _ = env.P[state][action][i]
-                action_value += prob * (r + lmbda * stateValue[next_state])
+            for i in range(len(env.env.P[state][action])):
+                prob, next_state, r, _ = env.env.P[state][action][i]
+                action_value += prob * (r + gamma * stateValue[next_state])
             action_values.append(action_value)
         best_action = np.argmax(np.asarray(action_values))
         policy[state] = best_action
@@ -69,9 +76,9 @@ def get_score(env, policy, episodes=1000):
     print('And you fell in the hole {:.2f} % of the times'.format((misses / episodes) * 100))
     print('----------------------------------------------')
 
+if __name__ == '__main__':
+    env = gym.make('FrozenLake-v0')
 
-env = gym.make('FrozenLake8x8-v0')
-
-stateValues = value_iteration(env, max_iterations=100000)
-policy = get_policy(env, stateValues)
-get_score(env, policy, episodes=1000)
+    stateValues = value_iteration(env, max_iterations=100000)
+    policy = get_policy(env, stateValues)
+    get_score(env, policy, episodes=1000)
--- FrozenLake/main_frozenlake.py
+++ FrozenLake/main_frozenlake.py
@@ -6,9 +6,6 @@ import numpy as np
 import gym
 import pandas as pd
 import seaborn as sns
-env = gym.make('FrozenLake-v0') # https://gym.openai.com/envs/FrozenLake-v0/
-print(env.action_space) # print action space for cartpole
-print(env.observation_space) # print state space for cartpole
 from itertools import product # cartesian product
 from tqdm import tqdm
 
@@ -23,12 +20,17 @@ def create_q_table(n_obs_space, n_action_space):#
     q_table_keys = [element for element in product(*[list(range(n_obs_space)),
                                                 list(range(n_action_space))])]
     #q_table = dict(zip(q_table_keys,np.random.uniform(0, 1, len(q_table_keys))))
-    q_table = dict(zip(q_table_keys,[0.5]*len(q_table_keys)))
+    q_table = dict(zip(q_table_keys,[0]*len(q_table_keys)))
 
     return q_table
 
+def create_s_table(n_obs_space):
 
-def choose_action(q_table, env, state, epsilon):
+    s_table = dict(zip(range(n_obs_space),[0.5]*n_obs_space))
+
+    return s_table
+
+def choose_action(qtable, env, state, epsilon):
     '''
 
     :param q_table: (dict)
@@ -37,14 +39,13 @@ def choose_action(q_table, env, state, epsilon):
     :param epsilon: (float)
     :return:
     '''
-    random_number = np.random.rand() # generate random number
+    random_number = np.random.uniform(0,1) # generate random number
 
     if random_number<=epsilon: # explore
         action = env.action_space.sample()
 
-    else: # choose action idnex with the max q value
-        q_table_keys = [(state,action) for action in range(env.action_space.n)] # q vals for all actions on that state
-        action = np.array([q_table[k] for k in q_table_keys]).argmax() # action that has the highest q val
+    else: # choose action index with the max q value
+        action = np.argmax(qtable[state, :])
 
     return action
 
@@ -66,7 +67,7 @@ def update_q_table_doesnt_work(q_table, experiences):
     return q_table
 
 
-def update_q_table(q_table, experiences, gamma, learning_rate):
+def update_q_table_doesnt_work_well(q_table, experiences, gamma, learning_rate):
     '''
         Implementation of the temporal difference learning update:
     Q(s,a) <-- Q(s,a) + alpha * [target - prediction].
@@ -97,13 +98,42 @@ def update_q_table(q_table, experiences, gamma, learning_rate):
             next_state_keys = [(state,action) for action in range(env.action_space.n)]
             next_state_qvals = [q_table[k] for k in next_state_keys]
             target = reward + gamma * np.max(next_state_qvals)
-        prediction = q_table[state,action] # existing q value
+        prediction = q_table[(state,action)] # existing q value
         updated_q_val = prediction + learning_rate * (target - prediction)
         # update the q value
         q_table[state, action] = updated_q_val
 
+
     return q_table
 
+
+def update_q_table(q, gamma, learning_rate):
+    '''
+        Implementation of the temporal difference learning update:
+    Q(s,a) <-- Q(s,a) + alpha * [target - prediction].
+    where:
+    prediction = Q(s,a),
+    and
+    target = r + gamma * max_a'[Q(s',a')] for Q-learning,
+    or
+    target = r + gamma * [ (1-epsilon)* max_a'[Q(s',a')] +
+                           epsilon* mean[Q(s',a')] for SARSA.
+
+    The definition of the target changes depending on whether the learning is done
+    off-policy (Q-Learning) or on-policy (SARSA).
+    Off-policy (Q-Learning) computes the difference between Q(s,a) and the maximum
+    action value, while on-policy (SARSA) computes the difference between Q(s,a)
+    and the weighted sum of the average action value and the maximum.
+
+    :param q: (np.array)
+    :return: (dict) updated q (table)
+    '''
+
+    prediction = q[state, action]
+    target = reward + gamma * np.max(q[next_state, :])
+    q[state, action] = prediction + learning_rate * (target - prediction)
+
+    return q
 
 def visualise_q_table(q_table):
     '''
@@ -111,13 +141,7 @@ def visualise_q_table(q_table):
     :param q_table:(dict)
     :return:
     '''
-    states = list(set([k[0] for k in q_table.keys()]))
-    actions = list(set([k[1] for k in q_table.keys()]))
-    qtable_df= pd.DataFrame(index=states, columns=actions)
-    for key,value in q_table.items():
-        state= key[0]
-        action = key[1]
-        qtable_df.loc[state,action]=value
+    qtable_df= pd.DataFrame(q_table)
     sns.heatmap(qtable_df.astype(float), annot=True)
 
 
@@ -142,45 +166,53 @@ def adjust_learning_rate(lr, gradient_N, rewards):
 
 if __name__ == '__main__':
 
-    q= create_q_table(env.observation_space.n, env.action_space.n)
+    env = gym.make('FrozenLake-v0')  # https://gym.openai.com/envs/FrozenLake-v0/
+    print(env.action_space)  # print action space for cartpole
+    print(env.observation_space)  # print state space for cartpole
+
+    q= np.zeros((env.observation_space.n, env.action_space.n)) # state-action value table
     epsilon= 1
-    gamma = 0.9
-    learning_rate = 1
+    min_epsilon=0.01
+    max_epsilon=1
+    gamma = 0.99 # for this task the closer this is to 1, the more reliable is the avg reward
+    learning_rate = 0.8 # if this is >=1, the training overshoots
+    decay_rate = 1.5e-3
     n_episodes= 10000
     render=False
     rewards = list()
     durations = list()
+    epsilons = list()
 
     for i_episode in tqdm(range(n_episodes)):
         state = env.reset()
-        next_state = None
-        episode_experiences=list()
+
         for t in range(100):
-            state =  next_state if next_state is not None else state
             if render:
                 env.render()
-            #print('Cur state: ' + str(state) )
+            #Pick Action
             action = choose_action(q, env, state, epsilon)
-            #print('Cur action: ' + str(action))
             next_state, reward, done, info = env.step(action)
-            episode_experiences.append((state,action,next_state, reward, done))
+            # Update State-Action Value Table
+            q = update_q_table(q, gamma, learning_rate)
+
+            state=next_state
             if done:
                 if render:
                     print("Episode finished with reward= %f after %i timesteps"%(reward, t+1))
                 rewards.append(reward)
                 durations.append(t)
-                # Vanilla Q learning, avg. 50 episode reward oscillates a lot
-                q = update_q_table(q, episode_experiences, gamma, learning_rate)
-                epsilon = max(0.1,1-(i_episode/n_episodes)) # anneal epsilon with a floor of 5% random actions
-                #learning_rate = max(0.05,1-(i_episode/n_episodes))#adjust_learning_rate(learning_rate, 10, rewards)
+                epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate* i_episode)
+                epsilons.append(epsilon)
+                 #learning_rate = max(0.05,1-(i_episode/n_episodes))#adjust_learning_rate(learning_rate, 10, rewards)
                 break
 
 
     visualise_q_table(q)
 
+    pd.DataFrame(rewards).rolling(100).mean().plot()
     pd.concat([pd.DataFrame(rewards).rolling(100).mean(),
                pd.DataFrame(durations).rolling(100).mean()], axis=1).to_csv('exp_results.csv')
-
+    pd.DataFrame(epsilons).plot()
 
 
 def update_q_table(self, obs, action, reward, next_obs, done, func):
